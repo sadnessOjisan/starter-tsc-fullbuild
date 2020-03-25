84921e782164d2ec1b77c8b1324d2446bef0043e
sadness_ojisan <sadness.ojisan@gmail.com>
2020-03-26 04:21:50 +0900

    chore: test対象のコードを追加

---
+++ src/util/ua.ts
@@ -0,0 +1,15 @@
+const ua = navigator.userAgent.toLowerCase();
+
+// Safari
+const isSafari = () => ua.indexOf("safari") > -1 && ua.indexOf("chrome") == -1;
+
+// iPhone
+const isiPhone = () => ua.indexOf("iphone") > -1;
+
+// iPad
+const isiPad = () => ua.indexOf("ipad") > -1;
+
+/**
+ * iOS Safariかどうかを判定する
+ */
+export const isIOSSafari = () => (isiPad() || isiPhone()) && isSafari();
