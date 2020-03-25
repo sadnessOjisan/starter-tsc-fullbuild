668d0443d5cad9b614e6218f76b09ccccda2e130
sadness_ojisan <sadness.ojisan@gmail.com>
2020-03-25 23:57:36 +0900

    chore: html file add

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,36 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
-  mode: "none", // 結果を見やすくするため
+  mode: "development",
   entry: "./src/main.ts",
   output: {
     path: path.resolve(__dirname, "./dist"),
-    filename: "bundle.js"
+    filename: "build.js"
   },
   module: {
     rules: [
       {
-        test: /\.ts$/,
-        exclude: /(node_modules)/,
-        use: {
-          loader: "ts-loader"
-        }
+        test: /\.(js|ts|tsx)$/,
+        use: [
+          {
+            loader: "ts-loader"
+          }
+        ],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(css)$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png)$/,
+        use: ["file-loader"]
       }
     ]
-  }
+  },
+  resolve: {
+    extensions: [".js", ".ts", ".tsx", ".css", ".gif", "jpg", "png", "mp4"]
+  },
+  plugins: [new HtmlWebpackPlugin({ template: "./src/index.html" })] // templateを指定しないとhtmlにrootが入らない
 };
