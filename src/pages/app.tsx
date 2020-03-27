73b938fa478346fd9a2527eb8c00ce375d1b8944
sadness_ojisan <sadness.ojisan@gmail.com>
2020-03-27 15:35:48 +0900

    chore: react testing library

--- src/pages/app.tsx
+++ src/pages/app.tsx
@@ -1,5 +1,13 @@
 import * as React from "react";
 
-export const App = () => {
-  return <div>Hello, {process.env.REACT_APP_ENV}!!</div>;
+export const App: React.FC = () => {
+  const [isMounted, setMounted] = React.useState(false);
+  React.useEffect(() => {
+    setMounted(true);
+  }, []);
+  return (
+    <div data-testid="wrapper">
+      Hello, {process.env.REACT_APP_ENV}!! {isMounted ? "mounted" : "unmounted"}
+    </div>
+  );
 };
