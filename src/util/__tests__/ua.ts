3986f858566c240eeb2d03ffc73a953b59b4590f
sadness_ojisan <sadness.ojisan@gmail.com>
2020-03-27 15:43:36 +0900

    feat: precommit hook format

--- src/util/__tests__/ua.ts
+++ src/util/__tests__/ua.ts
@@ -1,27 +1,28 @@
 import { isIOSSafari } from "../ua";
 
-describe("ユーザーエージェントのテスト", () => {
+describe("uaのテスト", () => {
   test.each([
     [
       "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36",
-      false
+      false,
     ],
     [
       "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100",
-      false
+      false,
     ],
     [
       "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:68.0) Gecko/20100101 Firefox/68.0",
-      false
+      false,
     ],
     [
       "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.0 Safari/605.1.15",
-      false
+      false,
     ],
+    // iOS Safari
     [
       "Mozilla/5.0 (iPhone; CPU iPhone OS 12_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.0 Mobile/15E148 Safari/604.1",
-      true
-    ]
+      true,
+    ],
   ])("%sのとき、%pを返す", (ua, expected) => {
     const result = isIOSSafari(ua);
     expect(result).toBe(expected);
