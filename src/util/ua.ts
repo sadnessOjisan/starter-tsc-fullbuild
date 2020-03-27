a8fdf898e58ff702c9c6d18c40280d09dd2ae21c
sadness_ojisan <sadness.ojisan@gmail.com>
2020-03-27 15:23:51 +0900

    chore: parametrized test

--- src/util/ua.ts
+++ src/util/ua.ts
@@ -1,18 +1,17 @@
-const ua = navigator.userAgent.toLowerCase();
-console.log("outer", ua);
 // Safari
-const isSafari = () => ua.indexOf("safari") > -1 && ua.indexOf("chrome") == -1;
+const isSafari = (ua: string) =>
+  ua.indexOf("safari") > -1 && ua.indexOf("chrome") == -1;
 
 // iPhone
-const isiPhone = () => ua.indexOf("iphone") > -1;
+const isiPhone = (ua: string) => ua.indexOf("iphone") > -1;
 
 // iPad
-const isiPad = () => ua.indexOf("ipad") > -1;
+const isiPad = (ua: string) => ua.indexOf("ipad") > -1;
 
 /**
  * iOS Safariかどうかを判定する
  */
-export const isIOSSafari = () => {
-  console.log(ua);
-  return (isiPad() || isiPhone()) && isSafari();
+export const isIOSSafari = (ua: string) => {
+  const uaLower = ua.toLowerCase();
+  return (isiPad(uaLower) || isiPhone(uaLower)) && isSafari(uaLower);
 };
