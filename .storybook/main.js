97c0d8e80b7e1570a6244127fb762520c4ff3ff5
sadnessOjisan <sadness.ojisan@gmail.com>
2020-04-02 00:51:51 +0900

    chore: storybook path

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,9 @@
 module.exports = {
-  stories: ['../stories/**/*.stories.js'],
-  addons: ['@storybook/addon-actions', '@storybook/addon-links'],
+  stories: ["../src/**/__stories__/*.js"],
+  addons: ["@storybook/addon-actions", "@storybook/addon-links"],
   webpackFinal: async config => {
     // do mutation to the config
 
     return config;
-  },
+  }
 };
