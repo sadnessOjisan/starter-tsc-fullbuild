97969964cd02d13d70db95a632ed670bf05a6f9f
sadnessOjisan <sadness.ojisan@gmail.com>
2020-04-08 17:13:59 +0900

    feat: eslint, prettierのignore設定

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,11 @@ const Dotenv = require("dotenv-webpack");
 const path = require("path");
 
 module.exports = {
-  mode: "development",
+  mode: process.env.NODE_ENV,
   entry: "./src/main.tsx",
   output: {
     path: path.resolve(__dirname, "./dist"),
-    filename: "build.js"
+    filename: "build.js",
   },
   module: {
     rules: [
@@ -15,31 +15,31 @@ module.exports = {
         test: /\.(js|ts|tsx)$/,
         use: [
           {
-            loader: "ts-loader"
-          }
+            loader: "ts-loader",
+          },
         ],
-        exclude: /node_modules/
+        exclude: /node_modules/,
       },
       {
         test: /\.(css)$/,
-        use: ["style-loader", "css-loader"]
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.(png)$/,
-        use: ["file-loader"]
-      }
-    ]
+        use: ["file-loader"],
+      },
+    ],
   },
   resolve: {
-    extensions: [".js", ".ts", ".tsx", ".css", ".gif", "jpg", "png", "mp4"]
+    extensions: [".js", ".ts", ".tsx", ".css", ".gif", "jpg", "png", "mp4"],
   },
   devServer: {
     contentBase: path.join(__dirname, "dist"),
     compress: true,
-    port: 8000
+    port: 8000,
   },
   plugins: [
     new HtmlWebpackPlugin({ template: "./src/index.html" }),
-    new Dotenv()
-  ] // templateを指定しないとhtmlにrootが入らない
+    new Dotenv(),
+  ], // templateを指定しないとhtmlにrootが入らない
 };
