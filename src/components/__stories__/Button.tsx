c8f40966a5b32e4999aedcf9a12bb43423bec600
sadnessOjisan <sadness.ojisan@gmail.com>
2020-04-09 00:55:41 +0900

    fix: test cov build

--- src/components/__stories__/Button.tsx
+++ src/components/__stories__/Button.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { action } from "@storybook/addon-actions";
 import { linkTo } from "@storybook/addon-links";
-import { withKnobs, text, boolean, number } from "@storybook/addon-knobs";
+import { withKnobs, boolean } from "@storybook/addon-knobs";
 import { Button } from "../Button";
 import Icon from "../../assets/sample.png";
 
